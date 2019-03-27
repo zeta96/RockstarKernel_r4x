d6960bcd6997de19b4192e9532caffea028d5f29
dhruv <dhruvgera61@gmail.com>
2019-03-27 17:25:15 +0000

    GPU OC

--- drivers/clk/msm/clock-gcc-8952.c
+++ drivers/clk/msm/clock-gcc-8952.c
@@ -773,7 +773,6 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk_8937_475MHz[] = {
 	F_SLEW( 500000000, 1000000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 575000000, 1150000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 615000000, 1200000000,	  gpll3,	1,	0,	0),
-	F_SLEW( 620000000, 1240000000,	  gpll3,	1,	0,	0),
         F_SLEW( 640000000, 1280000000,	  gpll3,	1,	0,	0),
 	F_END
 };
@@ -4245,7 +4244,7 @@ static void override_for_8937(int speed_bin)
 		OVERRIDE_FMAX6(gfx3d,
 			LOWER, 160000000, LOW, 240000000,
 			NOMINAL, 320000000, NOM_PLUS, 400000000,
-			HIGH, 500000000, SUPER_TUR, 620000000);
+			HIGH, 575000000, SUPER_TUR, 615000000);
 		OVERRIDE_FTABLE(gfx3d, ftbl_gcc_oxili_gfx3d_clk, 8937_475MHz);
 	} else {
 		OVERRIDE_FMAX5(gfx3d,
@@ -4428,13 +4427,13 @@ static int msm_gcc_probe(struct platform_device *pdev)
 				gfx3d_clk_src.freq_tbl =
 					ftbl_gcc_oxili_gfx3d_clk_8940_500MHz;
 				gfx3d_clk_src.c.fmax[VDD_DIG_SUPER_TUR] =
-								620000000;
+								615000000;
 			} else {
 				gfx3d_clk_src.freq_tbl =
 					ftbl_gcc_oxili_gfx3d_clk_8937_475MHz;
 				gfx3d_clk_src.c.fmax[VDD_DIG_SUPER_TUR] =
 
-								620000000;
+								615000000;
 
 			}
 		}
